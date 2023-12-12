f45dabfd9530982a3c3dc8fd77ea9f3d94b93f17
michy06 <119665642+michy06@users.noreply.github.com>
2023-12-12 23:41:28 +0100

    GrafodiHolt.java

--- eclipse-workspace/it.edu.iisgubbio/src/tpsit/GrafodiHolt.java
+++ eclipse-workspace/it.edu.iisgubbio/src/tpsit/GrafodiHolt.java
@@ -1,39 +1,78 @@
 package tpsit;
 
-import javafx.application.Application;
 
 public class GrafodiHolt{
-	int amatrice [][];
-	int riga;
-	int colonna;
+	
+	//dichiarazione varibile e l'ammatrice 
+	int grafodiHolt [][];
+	int dimensioniGrafo;
 
-	public Graforisorse (  int riga , int colonna) {
-		
-
-		int amatrice [][] = new int [3][3];
-		for(int riga=0; riga.lenght;riga++) {
-		}
-		for(int colonna=0; colonna.lenght;colonna++) {
+	//Il costruttore inizializza una matrice bidimensionale ("grafodiHolt [][]")
+	//specificando le sue dimensioni tramite il paramtro ("dimensioniGrafo")
+	public GrafodiHolt(int dimensioniGrafo) {
+	
+		this.grafodiHolt =  new int[dimensioniGrafo][dimensioniGrafo];
+		this.dimensioniGrafo=dimensioniGrafo;
+		for (int k= 0; k< this.dimensioniGrafo; k++) {
+			for (int j = 0; j < this.dimensioniGrafo; j++) {
+				this.grafodiHolt[K][j]=0;
+			}
 			
-			 amatrice [riga][colonna] = 0;
 		}
 
 	}
-	public static void main(String[] args) {
+	//il metodo "checkcolum" verifica se la somma degli elementi nella colonna resources è uguale a 0.
+	//restituisce true se la somma è 0 altrimenti restituisce false.
+	public Boolean checkcolum(int resources) { 
 		
-	
-	
-	public void setRequest(int processo int risorsa) {
-		if(this.grafo[processo][risorsa]==0) {
-			this.grafo[processo][risorsa]=1;
-		}else {
-		 
-			this.grafo[processo][risorsa]=2;
+		int somma = 0;
+		for (int k = 0; k < this.dimensioniGrafo; k++) {
+			somma = somma  + this.grafodiHolt[k][resources];
+		}
+		if (somma == 0) {
+			return (true);
+		}
+		else {
+			return(false);
 		}
 	}
+	//Il metodo "setRequest" imposta un'assegnazione di risorsa per i parametri processo e risorsa.
+	
+	//Altrimenti, viene assegnato il valore 2. Se l'assegnamento non è possibile, viene stampato un messaggio di errore.
+	public void setRequest(int processo, int risorsa) {
+	
+
+		if (this.grafodiHolt[processo][risorsa]==0) {
+			if (this.checkcolum(risorsa)){
+				this.grafodiHolt[processo][risorsa]=1;
+				//Se l'elemento della matrice corrispondente a 0 e la colonna (risorsa) ha una somma di 0 
+				//(verificato tramite checkcolum) viene assegnato il valore 1.
+			}
+			else {
+				this.grafodiHolt[processo][risorsa]=2;
+				//Altrimenti, viene assegnato il valore 2 
+			}
+		}
+		else {
+			System.out.print("Errore assegnamento non possibile");
+			//Se l'assegnamento non è possibile, viene stampato un messaggio di errore.
+		}
+
+	}
+	// il metodo "print2D" verifica se stampa la matrice bidimensionale  sulla console.
+	public  void print2D() {
+		
+		for (int k = 0; k < this.dimensioniGrafo; k++) {
+			for (int j = 0; j < this.dimensioniGrafo; j++) {
+				System.out.print(this.grafodiHolt[k][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+
+
 
 }
-}   
 
 
 
