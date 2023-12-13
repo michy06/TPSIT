ee5a1002114a03ce801c342d4a450f7c2c433e11
michy06 <119665642+michy06@users.noreply.github.com>
2023-12-13 09:39:48 +0100

    fcfs.

--- eclipse-workspace/it.edu.iisgubbio/src/tpsit/fcfs.java
+++ eclipse-workspace/it.edu.iisgubbio/src/tpsit/fcfs.java
@@ -3,8 +3,8 @@ public class fcfs {
 
 	public static void main(String[] args ) {
 		
-		// Dichiarazione e inizializzazione degli array e variabile media per:
-	    //i processi, i tempi di arrivo, esecuzione, servizio e attesa
+	     // Declaration and initialization of arrays and average variable for:
+	    //processes, arrival, execution, service and waiting times
 		String[] processo= new String[4];
 		int []tArrivo=new int [4];
 		int []tEsecuzione=new int [4];
@@ -13,7 +13,7 @@ public class fcfs {
 		double media=0;
 		
 		
-		// Inizializzazione dei nomi dei processi
+		// Initialization of process names
 		processo[0]="P0";  
 		processo[1]="P1";
 		processo[2]="P2";
@@ -21,16 +21,16 @@ public class fcfs {
 
 		
 		//1) Ciclo For: 
-		//rappresentare i tempi di arrivo dei processi nel sistema tramite l'assegniazione di valori crescenti
-		// a "tArrivo" che gli viene assegnato il valore "X". Ogni volta che il For terminerà il suo giro conterà i seguenti 
-		// valori "[0, 1, 2, 3,]"
+		//to represent the arrival times of the processes in the system through the allocation of increasing values
+		// to "tArrivo" which is assigned the value "X". Each time the For finishes its turn it will count the following 
+		// values "[0, 1, 2, 3,]"
 		
 		for (int x=0; x<4;x++) { 
 			tArrivo[x]=x;
 		}
 		
 	
-		// Inizializzazione dei tempi di esecuzione
+		// Initialization of execution times
 		tEsecuzione[0]=5; 
 		tEsecuzione[1]=3;
 		tEsecuzione[2]=8;
@@ -38,45 +38,45 @@ public class fcfs {
 
 
 		//2) Ciclo For: 
-		//calcola il tempo di servizio , di attesa e la media
+		//calculate service time , waiting time and average
 		for(int k=0; k<4;k++) {
 			
-			//"K" rappresenta i processi che scorrono grazie al ciclo For 
+			//"K" represents the processes that flow thanks to the Ciclo For
 			if(k==0) { 
 				tServizio[k]=0;
 				tAttesa[k]=0;
-				//"K==0" essendo il primo porcesso ha i tempi di servizio e d'attesa uguali a 0 
+				//"K==0" the first porcess has the service and waiting times equal to 0 
 		
 			}else {
 				tServizio[k]=tServizio[k-1]+tEsecuzione[k-1];
-				//Il tempo di servizio è calcolato:
-				//dalla somma del tempo di servizio del processo precedente e il tempo di esecuzione del processo in esecuzione
+				//The working time is calculated:
+				//from the sum of the service time of the previous process and the execution time of the running process
 				tAttesa[k]=tServizio[k]-tArrivo[k];
-				//Il tempo di servizio è calcolato:
-				//dalla differenza tra il tempo di servizio e il tempo d'arrivo del processo in esecuzione
+				//The working time is calculated:
+				//the difference between the service time and the arrival time of the running process
 				
 				media=media+tAttesa[k]/tAttesa.length;
-				//la media ogni giro di Ciclo For viene aggiornata 
-				//sommando il rapporto tra il tempo d'attese del processo in esecuzione e la lunghezza di "tAttesa"
+				//the average every round of Ciclo For is updated 
+				//summing the ratio between the waiting time of the running process and the length of "tAttesa"
 			}
 
 		}
-		// Stampa i nomi dei processi
+		// Print the process names
 		for (int i=0; i<=3;i++) {             
 			System.out.println(processo[i]);
 
 		}
-		// Stampa i tempi di servizio
+		// Print the service times
 		for (int p=0; p<=3;p++) {
 			System.out.println(tServizio[p]);
 
 		}
-		// Stampa i tempi di attesa
+		// Print the waiting times
 		for (int l=0; l<=3;l++) {
 			System.out.println(tAttesa[l]); 
 
 		}
-		// Stampa la media 
+		// Print the media 
 		System.out.println("la media è : "+media);
 
 	}
