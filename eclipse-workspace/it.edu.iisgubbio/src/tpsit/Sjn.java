2cea809f6877e2ba9db032a3964ecba89b4cb09f
michy06 <119665642+michy06@users.noreply.github.com>
2023-12-13 09:24:57 +0100

    Sjn

--- eclipse-workspace/it.edu.iisgubbio/src/tpsit/Sjn.java
+++ eclipse-workspace/it.edu.iisgubbio/src/tpsit/Sjn.java
@@ -5,8 +5,8 @@ public class SJN {
 
 
 	public static void main(String[] args) {
-		// Dichiarazione e inizializzazione degli array e variabile mediaAttesa per:
-	    //i processi, i tempi di arrivo, esecuzione, servizio e attesa
+	     // Declaration and initialization of arrays and variable "mediaAttesa" for:
+	    //processes, arrival, execution, service and waiting times
 		String [] processo = new String [4];
 		int [] arrivo = new int [4];
 		int [] esecuzione = new int [4];
@@ -14,44 +14,44 @@ public class SJN {
 		int [] attesa = new int [4];
 
 		double mediaAttesa;
-		// Inizializzazione dei nomi dei processi
+		// Initialization of process names
 		processo[0] = "P0";
 		processo[1] = "P1";
 		processo[2] = "P2";
 		processo[3] = "P3";
 		
 		//1) Ciclo For: 
-		//I tempi di arrivo vengono inizializzati con valori crescenti da 0 a 3 nell'array ("arrivo").
+		//Arrival times are initialized with values increasing from 0 to 3 in the array ("arrivo").
 		for(int x = 0; x<4; x++) {
 			arrivo[x]=x;
 		}
 		
 		
-		// Inizializzazione dei tempi di esecuzione
+		// Initialization of execution times
 		esecuzione[0] = 5;
 		esecuzione[1] = 3;
 		esecuzione[2] = 8;
 		esecuzione[3] = 6;
 		
 		
-		//Viene utilizzato l'algoritmo di ordinamento a bolle (Bubble sort) 
-		//con l'obiettivo di ordinare i tre array :
-		//("processo") ("arrivo")("esecuzione") in base ai tempi di esecuzione dei processi in ordine crescente
+		//The bubble sorting algorithm is used (Bubble sort) 
+		//with the goals to sort the three arrays :
+		//("processo") ("arrivo")("esecuzione") according to the execution times of the processes in ascending order
 		
 		int ultimo = esecuzione.length-1;  
-		//Viene calcolato l'indice dell'ultimo elemento dell'array ("esecuzione") 
+		//The index of the last element of the array is calculated ("esecuzione") 
 		
 		
-		//troviamo 2 Cicli for necessari per eseguire l'algoritmo 
+		//we find 2 Cicli For for needed to run the algorithm 
 		for(int superficie=0;superficie<=ultimo-1;superficie=superficie+1){
-			//1) Ciclo For ripete attraverso l'array fino al penultimo elemento.
+			//1) Ciclo For repeats the array to the penultimate element.
 
 			for(int posizione=ultimo-1;posizione>=superficie;posizione--){
-			//2) Ciclo For ripete dall'ultimo elemento fino a superficie.
+			//2) Ciclo For repeats from last element to "superficie".
 				
 				if(esecuzione[posizione]>esecuzione[posizione+1]){
-				//questa condizione verifica se il tempo di esecuzione del processo corrente 
-				//è maggiore del tempo di esecuzione del processo successivo.	
+				//this condition checks if the running time of the current process 
+				//is greater than the execution time of the next process.	
 					
 					int appoggioEsecuzione=esecuzione[posizione];
 					String appoggioProcesso = processo[posizione];
@@ -64,22 +64,23 @@ public class SJN {
 					esecuzione[posizione+1]=appoggioEsecuzione;
 					processo[posizione+1]=appoggioProcesso;
 					arrivo[posizione+1]=appoggioArrivo;
-					//Se la condizione è vera, viene eseguita una "Sostituzione (swap)" degli elementi corrispondenti negli array.
-					//vengono scambiati i tempi di esecuzione, i nomi dei processi e i tempi di arrivo dei processi per posizionare 
-					//i processi in ordine crescente in base ai tempi di esecuzione.
+					//If the condition is true, a "swap" of the corresponding elements in the arrays is performed.
+					//Execution times, process names and process arrival times are exchanged in place 
+					//processes in ascending order according to execution times.
+
 				}
 			}
 		}
 
 
 		
-		// Questo Ciclo For calcola i tempi di servizio, d'attesa e la media dei tempi di attesa per i processi ordinati.
+		// This Ciclo For calculates the service times, waiting times and the average waiting times for the ordered processes.
 		for(int y = 0; y<4; y++) {
 			
-			//"y" rappresenta i processi che scorrono grazie al ciclo For 
+			//"y" represents the processes that flow thanks to the Ciclo For 
 			if(y==0) {
 				
-				//"y==0" essendo il primo porcesso ha i tempi di servizio e d'attesa uguali a 0 
+				//"y==0"  the first porcess has the service and waiting times equal to 0 
 				servizio[0]= 0;
 				attesa[0]=0;
 				
@@ -87,55 +88,54 @@ public class SJN {
 			}else {
 
 				servizio[y]=servizio[y-1]+esecuzione[y-1];
-				// i tempi di servizio vengono calcolati dalla somma:
-				//del tempo di esecuzione del processo precedente (esecuzione[y-1])
-				//e il tempo di servizio del processo precedente (servizio[y-1]).			
+				// service times are calculated from the sum:
+				//the execution time of the previous process (esecuzione[y-1])
+				//and the service time of the previous process (servizio[y-1]).			
 				
 				attesa[y] =  servizio[y]-arrivo[y];
-				//Il tempo di attesa viene calcolato sottraendo il tempo di arrivo del processo corrente (arrivo[y]) 
-				// dal tempo di servizio corrispondente.
+				//The waiting time is calculated by subtracting the arrival time of the current process (arrivo[y]) 
+				// from the corresponding service time.
 			}
 		}
 
 		mediaAttesa =(double)(attesa[0]+attesa[1]+attesa[2]+attesa[3])/4;
-		//Viene calcolata la media dei tempi di attesa per tutti i processi, dividendola per il numero totale di processi (4)
+		//The average waiting time for all processes is calculated, dividing it by the total number of processes (4)
 		
 		for(int i = 0; i<=3; i++) {
 			System.out.println(processo[i]);
-			//tramite un ciclo for andiamo a staampare i nomi dei processi ordinati.
+			//with the ciclo for Let’s go to print the names of the processes ordered.
 		}
 		
 		
-        //Stampa la stringa "processi".
+        //Print the string "processi".
 		System.out.println("processi");
 		
 		for(int i = 0; i<=3; i++) {
 			System.out.println(esecuzione[i]);
-			//Utilizza un for per far ripetere attraverso l'array esecuzione e stampare ciascun elemento
+			//Use a for to repeat through the execution array and print each item
 		}
 		
-		//Stampa la stringa "esecuzioni".
+		//Print the string "esecuzioni".
 		System.out.println("esecuzioni");
 		
 		
 		for(int i = 0; i<=3; i++) {
 			System.out.println(servizio[i]);
-			//Utilizza un for per far ripetere attraverso l'array esecuzione e stampare ciascun elemento
-		}
-        //Stampa la stringa "servizi".
+			//Use a for to repeat through the execution array and print each item
+        //Print the string "servizi".
 		System.out.println("servizi");
 		
 		for(int i = 0; i<=3; i++) {
 			System.out.println(attesa[i]);
-			//Utilizza un for per far ripetere attraverso l'array esecuzione e stampare ciascun elemento
+			//Use a for to repeat through the execution array and print each item
 		}
 		
-        //Stampa la stringa "attese".
+        //Print the string "attese".
 		System.out.println("attese");
 		
 		
 		System.out.println("la media attesa è: "+mediaAttesa);
-        //Stampa la media delle attese, che sembra essere memorizzata nella variabile mediaAttesa.
+        //Print the average of the expectations, which seems to be stored in the average mediaAttesa
 		
 	}
 	
